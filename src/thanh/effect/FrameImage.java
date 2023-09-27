42528508a6abb90f364c6a58c13de89976325b13
NgoHoangThanh <ngohoanthanh23042003@gmail.com>
2023-09-27 09:47:08 +0700

    FrameImage

--- src/thanh/effect/FrameImage.java
+++ src/thanh/effect/FrameImage.java
@@ -1,5 +1,6 @@
 package thanh.effect;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class FrameImage {
@@ -9,6 +10,33 @@ public class FrameImage {
         this.name= name;
         this.image = image;
     }
-    
+    public FrameImage(FrameImage frameImage) {
+        image = new BufferedImage(frameImage.getImageWitdh(),frameImage.getImageHeight(),frameImage.getImage().getType());
+        Graphics g = image.getGraphics();
+        g.drawImage(frameImage.getImage(),0 ,0, null );
+    }
+    public void draw (Graphics2D g2, int x, int y) {
+        g2.drawImage(image, x - image.getWidth()/2, y - image.getHeight()/2, null);
+    }
+
+    public int getImageWitdh() {
+        return image.getWidth();
+    }
+    public  int getImageHeight() {
+        return  image.getHeight();
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public BufferedImage getImage(){
+        return image;
+    }
+    public void setImage(BufferedImage image){
+        this.image = image;
+    }
 
 }
